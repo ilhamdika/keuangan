25e3b1e093f1034eaa04cb816d5a4c9b090e2c18
ilhamdika <93665288+ilhamdika@users.noreply.github.com>
2023-12-28 19:17:44 +0700

    feat: make saldo migration

---
+++ app/Models/Saldo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Saldo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nominal', 'total_pemasukan', 'total_pengeluaran'];
+}
